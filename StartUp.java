dc657bfccefe0f19b690197cba25c5e6dca11e4e
2025JGOZAR <148128498+2025JGOZAR@users.noreply.github.com>
2024-01-09 12:47:03 -0500

    Create StartUp.java

---
+++ StartUp.java
@@ -0,0 +1,7 @@
+# This is a basic workflow to help you get started with Actions
+import org.code.neighborhood.*;
+   public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+
+Painter blake = new Painter();
